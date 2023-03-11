b5c6f7c21a55df7d7a1126a1e8667cb1cf447a73
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-11 13:08:37 +0530

    [Vaibhav] Add:Folder Structre

---
+++ LineComparison/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LineComparison
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("Enter the value if x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value if x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value if y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value if y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Calculation lengthCal = new Calculation(x1, y1, x2, y2);
+            lengthCal.Length();
+        }
+
+    }
+}
