2c4ea99b8b7118c152c4118552042761b39be2fb
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-11 13:32:03 +0530

    [Vaibhav] Add:Folder Structure

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -15,8 +15,11 @@ namespace LineComparison
             int y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the value if y2: ");
             int y2 = Convert.ToInt32(Console.ReadLine());
-            Calculation lengthCal = new Calculation(x1, y1, x2, y2);
-            lengthCal.Length();
+            Calculation lengthCal1 = new Calculation(x1, y1, x2, y2);
+            double line1 = lengthCal1.Length();
+            Calculation lengthCal2 = new Calculation(5, 6, 7, 8);
+            double line2 = lengthCal2.Length();
+            lengthCal2.EqualityCheck(line1, line2);
         }
 
     }
